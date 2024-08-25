6b57176c5b853ac6689fabcf49b8f7cea11c3748
Burak Ozturk <burak.ozturk@ug.bilkent.edu.tr>
2024-08-25 17:04:22 +0300

    Deleting with POST and Postman Collection
    Custom post method for viewset to POST with {{url}}/{{objectName}}/ and DELETE with {{url}}/{{objectName}}/{{objectId}}
    
    also updated_postman_collection.json's only difference is some extra slashes at the end of some URLs
    
    tokens have very long lifetimes

--- aviation_project/api/views.py
+++ aviation_project/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework.response import Response
-from rest_framework import viewsets, serializers
+from rest_framework import viewsets, serializers, status
+from rest_framework.decorators import action
 
 from .models import Aircraft, Airline
 from .serializers import AircraftListSerializer, AircraftDetailedSerializer, AirlineListSerializer, AirlineDetailedSerializer, CustomTOPSerializer
@@ -22,6 +23,24 @@ class AirlineViewSet(BaseViewSet, viewsets.ModelViewSet):
 
         return Response(serializer.data)
 
+    def post(self, request, *args, **kwargs):
+        # If there is a pk (airline_id) in the URL, this code performs the POST deletion operation
+        pk = kwargs.get('pk')
+        if pk:
+            return self.delete_airline(request, pk)
+        else:
+            # If there is no pk, perform the normal creation operation
+            return self.create(request, *args, **kwargs)
+
+    @action(detail=True, methods=['post'])
+    def delete_airline(self, request, pk=None):
+        try:
+            airline = self.get_object()
+            airline.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Airline.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
 class AircraftViewSet(BaseViewSet, viewsets.ModelViewSet):
     queryset = Aircraft.objects.all()
     serializer_class = AircraftDetailedSerializer
@@ -33,6 +52,24 @@ class AircraftViewSet(BaseViewSet, viewsets.ModelViewSet):
         serializer = AircraftListSerializer(queryset, many=True, context={'request': request})
 
         return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        # If there is a pk (aircraft_id) in the URL, this code performs the POST deletion operation
+        pk = kwargs.get('pk')
+        if pk:
+            return self.delete_aircraft(request, pk)
+        else:
+            # If there is no pk, perform the normal creation operation
+            return self.create(request, *args, **kwargs)
+
+    @action(detail=True, methods=['post'])
+    def delete_aircraft(self, request, pk=None):
+        try:
+            aircraft = self.get_object()
+            aircraft.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        except Aircraft.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
 class CustomTOPView(TokenObtainPairView):
     serializer_class = CustomTOPSerializer
--- aviation_project/aviation_project/settings.py
+++ aviation_project/aviation_project/settings.py
@@ -134,7 +134,7 @@ REST_FRAMEWORK = {
 from datetime import timedelta
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=365),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=365),
     'AUTH_HEADER_TYPES': ('Bearer',),
 }
